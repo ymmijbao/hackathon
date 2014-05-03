1bafd3e07bd856b16b0e42ecc0a4d8d783ca68a5
szhong <szhong@berkeley.edu>
2014-05-03 06:52:38 -0500

    [VoicialReceiver.java] : Added a sample broadcast receiver.

---
+++ Voicial/src/com/handsoap/voicial/VoicialReceiver.java
@@ -0,0 +1,32 @@
+package com.handsoap.voicial;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+
+public class VoicialReceiver extends BroadcastReceiver {
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		Bundle bundle = intent.getExtras();
+		SmsMessage[] msgs = null;
+		String str = "";
+		
+		if (bundle != null) {
+			Object[] pdus = (Object[])bundle.get("pdus");
+			msgs = new SmsMessage[pdus.length];
+			for (int i = 0; i < msgs.length; i++) {
+				msgs[i] = SmsMessage.createFromPdu((byte[])pdus[i]);
+				if (i == 0) {
+					str += msgs[i].getDisplayOriginatingAddress() + " ";
+				}
+				
+				str += msgs[i].getMessageBody().toString();
+			}
+		}
+		
+		this.abortBroadcast();
+		System.out.println(str);
+	}
+}
